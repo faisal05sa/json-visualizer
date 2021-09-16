21fa58fbc35a7f933aaddaa546a302a56336886b
Faisal <faisal@arbisoft.com>
2021-09-16 18:07:06 +0500

    Used react-redux

--- src/App.js
+++ src/App.js
@@ -1,102 +1,78 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-toastify/dist/ReactToastify.css";
-import React, { Component } from "react";
-import { Container, Navbar, Row } from "react-bootstrap";
-import FormComponent from "./components/form";
-import OutputComponent from "./components/output";
+import React from "react";
+import { useSelector } from "react-redux";
+import { Container, Navbar, Nav, Row } from "react-bootstrap";
 import { ToastContainer } from "react-toastify";
 
-class App extends Component {
-  state = {
-    settings: {},
-    json: undefined,
-  };
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
-  constructor() {
-    super();
-    this.setState = this.setState.bind(this); //bind setState because it is passing down somewhere else
+import MyLogo from "./features/logo/logo";
+import FormComponent from "./features/form/form";
+import OutputComponent from "./features/output";
+import { manageDarkModeClasses } from "./app/utils";
 
-    this.toggleControls = [
-      { name: "darkMode", text: "Dark Mode", default: false },
-      { name: "monospaceFont", text: "Preserve Space", default: true },
-      {
-        name: "truncateStrings",
-        text: "Truncate Long Strings",
-        default: false,
-      },
-    ];
+function App() {
+  const darkMode = useSelector((state) => state.input.settings.darkMode);
+  manageDarkModeClasses(darkMode);
 
-    let settings = {};
-    this.toggleControls.forEach((control) => {
-      settings[control.name] = control.default;
-    });
-    this.setState(settings);
-  }
+  return (
+    <div className={"App " + (darkMode ? "bg-dark" : "bg-light")}>
+      <TheNavBar />
+      <MainContainer />
+      <ToastContainer
+        position="bottom-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover={false}
+      />
+    </div>
+  );
+}
 
-  render() {
-    return (
-      <div
-        className={
-          "App " + (this.state.settings.darkMode ? "bg-dark" : "bg-light")
-        }
-      >
-        <ToastContainer
-          position="bottom-center"
-          autoClose={1000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable={false}
-          pauseOnHover={false}
-        />
+function TheNavBar() {
+  return (
+    <Navbar
+      variant="dark"
+      style={{ backgroundColor: "#20232a", maxHeight: 75 }}
+    >
+      <Container fluid>
+        <Navbar.Brand>
+          <MyLogo />
+          JSON to HTML Visualizer
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto"></Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
-        <Navbar
-          variant="dark"
-          style={{ borderRadius: 0, backgroundColor: "#20232a" }}
-        >
-          <Container fluid>
-            <Navbar.Brand>
-              <img
-                src="logo.svg"
-                alt=""
-                width="30"
-                height="24"
-                className="d-inline-block align-text-top"
-              />
-              JSON to HTML Visualizer
-            </Navbar.Brand>
-          </Container>
-        </Navbar>
-
-        <div className="container-fluid">
-          <Row variant={this.state.settings.darkMode ? "dark" : "secondary"}>
-            <div className="col-12">
-              <FormComponent
-                state={this.state}
-                setState={this.setState}
-                toggleControls={this.toggleControls}
-              />
-            </div>
-            <div
-              className={
-                "col-12 " +
-                (this.state.settings.monospaceFont ? "text-monospace" : "")
-              }
-            >
-              <OutputComponent
-                data={this.state.json}
-                darkMode={this.state.settings.darkMode}
-                truncateStrings={this.state.settings.truncateStrings}
-              />
-            </div>
-          </Row>
+function MainContainer() {
+  const settings = useSelector((state) => state.input.settings);
+  return (
+    <div className="container-fluid">
+      <Row variant={settings.darkMode ? "dark" : "secondary"}>
+        <div className="col-12">
+          <FormComponent />
         </div>
-      </div>
-    );
-  }
+        <div
+          className={
+            "col-12 " + (settings.monospaceFont ? "font-monospace" : "")
+          }
+        >
+          <OutputComponent />
+        </div>
+      </Row>
+    </div>
+  );
 }
 
 export default App;
